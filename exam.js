0dbc0f953e8f6334261cdf9e77c8cb6b9d2b7b1e
itamar1996 <itay9919@gmail.com>
2024-09-12 10:00:01 +0300

    commit4 5

--- exam.js
+++ exam.js
@@ -1,24 +1,57 @@
-/*שאלות - בחרו 4 מתוך השישה:
-צור פונקציה המקבלת מערך של מספרים ומחזירה מערך המכיל רק את המספרים הזוגיים מהמערך הקודם 
-צור פונקציה המקבלת מחרוזת ומחזירה את כמות המילים המכילות ארבעה אותיות
-צור פונקציה המקבלת מערך דו ממדי והופכת אותו למערך חד מימדי
- */
-let arr = [1,2,3,4,5,6,7,8,9,10]
 
-let Mission1 = (numarr)=>{
-    let evennumsarr = numarr.filter(num=>num%2==0);    
-    return evennumsarr;
-}
+let Mission1 = (numarr) => {
+  let evennumsarr = numarr.filter((num) => num % 2 == 0);
+  return evennumsarr;
+};
 
-let Mission2 = (str) =>{
-    let wordsarr = str.split(" ");
-    let count = 0;
-    for (let i = 0; i < wordsarr.length; i++) {
-        if(wordsarr[i].length == 4)
-        {
-            count++;
-        }
-    }   
-    return count;
+let Mission2 = (str) => {
+  let wordsarr = str.split(" ");
+  let count = 0;
+  for (let i = 0; i < wordsarr.length; i++) {
+    if (wordsarr[i].length == 4) {
+      count++;
+    }
+  }
+  return count;
+};
+
+
+let Mission5 = (keysarr, valuesarr) => {
+  let obj = new Object();
+  for (let i = 0; i < keysarr.length; i++) {
+    obj[keysarr[i]] = valuesarr[i];
+  }
+  console.log(obj);
+  return obj;
+};
+
+let Comparingarr = (arr1, arr2) => {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] != arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+let Mission4 = (arr) => {
+    let sortarr = [...arr].sort((a, b) => a - b);
+    let reversesortarr = [...arr].sort((a, b) => b - a);
+    
+    if (Comparingarr(arr, sortarr)) {
+        return 1;
+    } else if (Comparingarr(arr, reversesortarr)) {
+        return 2;
+    } else {
+        return 0;
+    }
+};
+
+
+module.exports ={
+    Mission1,
+    Mission2,
+    Mission4,
+    Mission5
 }
 
